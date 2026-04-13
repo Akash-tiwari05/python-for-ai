f21a1261fab153c8e65253c9df7f587b9bc5e63e
Akash-tiwari05 <myselfakashtiwari05@gmail.com>
2026-04-13 11:50:24 +0530

    strings in python

---
+++ python-for-ai/variables/String.py
@@ -0,0 +1,34 @@
+#Strings
+
+name = "My name is Akash"
+print(type(name),name)
+print(name)
+
+# Single quotes
+first = 'Python'
+
+# Double quotes  
+second = "Python"
+
+# Triple quotes for multiple lines
+paragraph = """This is
+a multi-line
+string"""
+print(paragraph)
+
+#Combining strings
+first_name = "Akash"
+last_name = "Tiwari"
+
+## Concatenation
+full_name = first_name+ " "+ last_name
+print(full_name)
+
+## Repetition
+stars = "*"*5
+print(stars)
+
+#String slicing
+str = "Akash"
+str = str[1:] #kash
+print(str)
---
+++ python-for-ai/variables/String2.py
@@ -0,0 +1,14 @@
+#Length
+message = "Hello"
+print(len(message))  # 5
+
+empty = ""
+print(len(empty))    # 0
+
+#Converting to string
+age = 25
+message = "I am " + str(age) + " years old"
+print(message)  # I am 25 years old
+
+# Or use f-strings
+message = f"I am {age} years old"
