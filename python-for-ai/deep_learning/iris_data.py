3f87de030fed8f008dc04882fcd63e8e90a37b4b
Akash-tiwari05 <myselfakashtiwari05@gmail.com>
2026-05-19 20:10:15 +0530

    iris data classification using nular network

--- python-for-ai/deep_learning/iris_data.py
+++ python-for-ai/deep_learning/iris_data.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
-
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
 
 # load dataset
 df = pd.read_csv(
@@ -28,28 +29,38 @@ y_encoded = encoder.fit_transform(y.values.reshape(-1, 1))
 
 #print(y_encoded)
 
-# split data
+ #Split both features (X) and the encoded target (y_encoded)
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y_encoded, test_size=0.2, random_state=42
 )
-
 
 # scale
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-
 # neural network
-model = MLPClassifier(
-    hidden_layer_sizes=(10,),
-    max_iter=1000,
-    random_state=42
+model = Sequential([
+    Dense(16, activation='relu', input_shape=(4,)),
+    Dense(12, activation='relu'),
+    Dense(3, activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
 )
 
-model.fit(X_train, y_train)
+# Train the model correctly and save the history
+history = model.fit(
+    X_train, 
+    y_train, 
+    epochs=50,          # Set number of training loops
+    batch_size=8,       # Set batch size
+    validation_data=(X_test, y_test) # Track test accuracy during training
+)
 
-# predictions
-y_pred = model.predict(X_test)
-
-print("Accuracy:", accuracy_score(y_test, y_pred))
+loss, accuracy =model.evaluate(X_train,y_test)
+print("Test Accuracy: ", accuracy)
+print("Test loss: ",loss)
